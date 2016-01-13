2197920eaf86f257914c191eaf1ce2ae30b73081
mickmason <mmason2@hotmail.com>
2016-01-13 23:33:12 +0000

    work on gamescene, landingscene, refactoring into scenes

---
+++ globals.lua
@@ -0,0 +1,103 @@
+-------------------------------------------
+-- ** Global properties and functions ** --
+-------------------------------------------
+local globals = {
+    functions = {
+        readJsonFile = function(fileName)
+            local path = system.pathForFile( fileName)
+            local file, errorString = io.open( path, "r" )
+            if not file then
+                    return errorString
+            else
+                    local json = require 'json'
+                    local jsonTable = json.decode(file:read( "*a" ))
+                    io.close(file)
+                    return jsonTable
+            end
+            file = nil
+        end,
+        writeJsonFile = function(fileName, data)
+            print('write file')
+            local path = system.pathForFile( fileName)
+            local file, errorString = io.open( fileName, "w" )
+            if not file then
+                        return errorString
+            else
+                local json = require 'json'
+                local jsData = json.encode(data)
+                file:write(jsData)
+                io.close(file)
+            end
+            file = nil
+            return true
+        end,
+        loadScoresData = function(data, username)
+            local playerData = data
+            local userScores
+            if err then
+                print("Error "..err)
+                return err
+            end        
+
+            for k, users in pairs(playerData) do
+                for i in pairs(users) do
+                    for l, v in pairs(users[i]) do
+                        if (l == 'username' and v == username) then
+                            userScores = users[i].scores
+                        end
+                    end
+                end
+            end    
+            return userScores
+        end,
+        updateScoresData = function(userScores, score)
+            local highScoreIndex = 0
+            local userScore = tonumber(score)
+            for i in ipairs(userScores) do
+                if (userScore > userScores[i] or userScore == userScores[i]) then
+                    highScoreIndex = i
+                    break
+                end
+            end
+            if (highScoreIndex >= 1) then 
+                table.insert(userScores, highScoreIndex, userScore)
+                table.remove(userScores, #gameScoresData.userScores)
+            end
+            for i in ipairs(userScores) do
+               print('line 231 '..userScores[i])
+            end
+            return userScores
+        end, 
+        updatePlayerData = function(playerData, userScores, username)
+            for j in ipairs(playerData) do
+                for i in pairs(playerData[j]) do
+                    for l, v in pairs(playerData[j]) do
+                        print(l)
+                        if (l == 'username' and v == username) then
+                            playerData[j][i].scores = userScores
+                        end
+                    end
+                end
+            end    
+            return playerData
+        end
+    },
+    dimensions = {
+        displayWidth = display.contentWidth,
+        displayHeight = display.contentHeight,
+        displayCenter = {
+            x = display.contentWidth * 0.5,
+            y = display.contentHeight * 0.5
+        },
+        pagePadding = {
+            top = 20, bottom = 10, left = 15, right = 15
+        }
+    },
+    fonts = {
+        sysFonts = native.getFontNames(),
+        gameBaseFont = 'Courier'
+    }
+}
+
+return globals
+
--- landingscene.lua
+++ landingscene.lua
@@ -1,78 +1,102 @@
+------------------------------
+-- ** Landing page scene ** --
+------------------------------
+
+local globals = require("globals")
 local composer = require('composer')
+
+-- New scene
 local scene = composer.newScene()
 
+-- Scene-wide objects and functions
+-- an array of all display object groups - used in scene:destroy() to remove them    
+local displayObjectGroups = {}
+-- global dimensions
+local dims = globals.dimensions
+-- global functions
+local funcs = globals.functions
+
+player = {
+    username = 'michael__'
+}
+-- player and game scores data
+local playerData = nil
+local userScoresData = {
+        userScores = nil
+}     
+-- Data error flag
+local dataError
+
+-- Will be display objects 
+local scoresText = {}
 
 -- scene:create()
 function scene:create(event)
     -- ** Landing page group ** --
     local sceneGroup = self.view
     -- Display objects --
-        
-    -- an array of all display object groups - used in scene:destroy() to remove them    
-    displayObjectGroups = {}
-    
+
     --landing page wrapper group
     local landingPageGroup = display.newGroup() 
     sceneGroup:insert(landingPageGroup)
-    landingPageGroup.width = pageWidth --5px padding on either side
-    landingPageGroup.height = displayHeight - 30 --30px padding on the top of the game area
     
-    table.insert(displayObjects, landingPageGroup)
+    table.insert(displayObjectGroups, landingPageGroup)
+    
+    landingPageGroup.width = pageWidth --5px padding on either side
+    landingPageGroup.height = dims.displayHeight - dims.pagePadding.top --30px padding on the top of the game area
+    
+    table.insert(displayObjectGroups, landingPageGroup)
 
     --Top scores table group
     local topScoresGroup = display.newGroup() 
     landingPageGroup:insert(topScoresGroup)
     
-    table.insert(displayObjects, topScoresGroup)    
+    table.insert(displayObjectGroups, topScoresGroup)    
     
-    local topScoresBg = display.newRect(displayCenter.x, 0, displayWidth*0.4, 0)
-    topScoresBg.y = displayCenter.y - topScoresBg.height*0.5 + pagePadding.top
+    local topScoresBg = display.newRect(dims.displayCenter.x, 0, dims.displayWidth*0.4, 0)
+    topScoresBg.y = dims.displayCenter.y - topScoresBg.height*0.5 + dims.pagePadding.top
     topScoresBg:setFillColor(0.3, 0.3, 0.3)
     
     --Play button group
     local playBttnGroup = display.newGroup()
     landingPageGroup:insert(playBttnGroup)
     
-    table.insert(displayObjects, landingPageGroup)    
+    table.insert(displayObjectGroups, landingPageGroup)    
     
-    local playBttn = display.newRect(displayCenter.x, 0, topScoresBg.width, 35)
+    local playBttn = display.newRect(dims.displayCenter.x, 0, topScoresBg.width, 35)
     playBttn:setFillColor(0.3, 0.3, 0.3)
     playBttnGroup:insert(playBttn)
     
-    playBttnText = display.newText({parent=playBttnGroup, text='Play!', font=gameBaseFont, fontSize=22, height=35, align='center', width=playBttn.width})
+    playBttnText = display.newText({parent=playBttnGroup, text='Play!', font=globals.fonts.gameBaseFont, fontSize=22, height=35, align='center', width=playBttn.width})
     
-    -- top scores data - player data
-    player = {
-        username = 'michael__'
-    }
-    local playerData = nil
-    local gameScoresData = {
-        userScores = {
-        }
-    }     
+
     -- Data and objects to show it --
-    playerData, dataError = loadScoresData()
+    playerData, dataError = funcs.readJsonFile('player_data/player_data.json')
     if (dataError) then
             print(dataError)
-            topScoresTxt = display.newText({parent=topScoresGroup, text='Sorry there was an error'..err, font=gameBaseFont, fontSize=22, align='left', width=topScores.width, height=200})
+            topScoresTxt = display.newText({parent=topScoresGroup, text='Sorry there was an error'..err, font=globals.fonts.gameBaseFont, fontSize=22, align='left', width=topScoresBg.width, height=200})
             topScoresBg.height = topScoresTxt.height +20
     else 
-        local scoresText = {}
-        for i in ipairs(gameScoresData) do
-            local scoresText[i] = display.newText({parent=topScoresGroup, text=player.username..'\t'..gameScoresData[i], font=gameBaseFont, fontSize=16, align='left', width=topScores.width, height=20})
+        userScoresData =  funcs.loadScoresData(playerData, player.username)
+
+        for i in ipairs(userScoresData) do
+            scoresText[i] = display.newText({parent=topScoresGroup, text=player.username..'\t'..userScoresData[i], font=globals.fonts.gameBaseFont, fontSize=16, align='left', width=topScoresBg.width, height=20})
             topScoresBg.height = topScoresBg.height + scoresText[i].height
         end
-    end     
-    
- 
+    end      
 end
 
 --scene:show()
 function scene:show(event)
     local sceneGroup = self.view
     local phase = event.phase
-    if (phase = 'will') then
-        playerData, dataError = loadScoresData()
+    if (phase == 'will') then
+--        playerData, dataError = loadScoresData()
+
+        if (event.params) then
+            --print('Event params '..event.params.someKey)
+        end
+
         if (dataError) then
             topScoresTxt.y = topScoresBg.y
             playBttn.isVisible, playBttnText.isVisible = false, false
@@ -80,20 +104,20 @@ function scene:show(event)
             for i in ipairs(scoresText) do
                 scoresText[i].x,scoresText[i].y = topScoresBg.x, topScoresBg.y + (topScoresBg.height * 0.5)+ (scoresText.height * #scoresText)
             end
-            playBttn.x, playBttn.y = displayCenter.x, topScoresBg.y + topScoresBg*0.5 + 20
+            playBttn.x, playBttn.y = dims.displayCenter.x, topScoresBg.y + topScoresBg*0.5 + 20
             playBttnText.x, playBttnText.y = playBttn.x, playBttn.y
         end   
     elseif (phase == 'did') then
         if (not dataError) then
-            function playBttnTapListener(event) 
+            function playBttnTapListener(e) 
                 local options = {
                     effect = 'fade',
                     time = 500,
                     params = {
-                        scoresData = gameScoresData
+                        scoresData = userScoresData
                     }
                 }
-                composer.goToScene('gamescene', options)
+                composer.gotoScene('gamescene', options)
             end
             playBttn.tap = playBttnTapListener
             playBttn:addEventListener('tap', playBttn)
@@ -105,7 +129,7 @@ end --end scene:show()
 function scene:hide(event)
         local sceneGroup = self.view
         local phase = event.phase
-        if (phase = 'will') then
+        if (phase == 'will') then
             playBttn:removeEventListener('tap', playBttnTapListener)
         end
 end--end scene:hide()
@@ -117,3 +141,11 @@ function scene:destroy()
         display.remove(group)
     end
 end
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+return scene
+
